00f54905645c4db394631e08483604ac8e930a38
Greg Funk <greg.funk@walltowall.com>
2015-11-18 16:06:27 -0500

    Downloads and displays a food image

--- app/src/main/java/com/example/gregfunk/androidguessthefruitorvegetable/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androidguessthefruitorvegetable/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.gregfunk.androidguessthefruitorvegetable;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -45,13 +51,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                InputStream in = urlConnection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(in);
+                return myBitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    ImageView imageView;
     ArrayList<String> foodURLs = new ArrayList<String>();
     ArrayList<String> foodNames = new ArrayList<String>();
+    int chosenFood = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         DownloadTask task = new DownloadTask();
         String result = null;
@@ -76,6 +110,15 @@ public class MainActivity extends AppCompatActivity {
             while (m3.find()) {
                 foodNames.add(m3.group(1));
             }
+
+            Random random = new Random();
+            chosenFood = random.nextInt(foodURLs.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap foodImage = imageTask.execute("http://www.greatgrubclub.com/" + foodURLs.get(chosenFood)).get();
+
+            imageView.setImageBitmap(foodImage);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
